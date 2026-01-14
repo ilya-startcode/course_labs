0b64d12c204c34006112a8eb0ef84809defd90af
ilya <stratienko_ilya03@mail.ru>
2026-01-14 09:34:38 -0500

    lab5: add requirements.txt & Flask-wrapper for typer script

--- labs/lab05/source/hello.py
+++ labs/lab05/source/hello.py
@@ -1,15 +1,16 @@
 import base64
-import typer
+from flask import Flask, request
 
+app = Flask(__name__)
 
-def main(
-    name: str = typer.Argument(...),
-    lastname: str = typer.Option("", "--lastname", "-l"),
-) -> None:
+@app.route("/")
+def index():
+    name   = request.args.get("name",   default="user", type=str)
+    lastname = request.args.get("lastname", default="",   type=str)
     greeting = base64.b64decode(b"SGVsbG8gYXBwc2Vjd29ybGQ=").decode()
-    tail = f"@{name}" + (f" {lastname}" if lastname else "")
-    typer.echo(f"{greeting} from {tail}")
-
+    tail     = f"@{name}" + (f" {lastname}" if lastname else "")
+    return f"{greeting} from {tail}\n"
 
 if __name__ == "__main__":
-    typer.run(main)
+    app.run(host="0.0.0.0", port=5000)
+EOF
